2820e1e127ad95f18ea70973598b2118082c1920
kirubak492 <kirubak492@gmail.com>
2023-09-07 23:02:23 +0530

    Bubble sort program

---
+++ Selection.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Selectionsort
+{
+	static void Select(int arr[])
+	{
+		for(int i=0;i<arr.length-1;i++)
+		{
+			int min=i;
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[j]<arr[min])
+				{
+					min=j;
+				}
+			}
+			int temp=arr[i];
+			arr[i]=arr[min];
+			arr[min]=temp;
+		}
+		return;
+		
+	}
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+			
+		}
+		Select(arr);
+		System.out.print(Arrays.toString(arr));
+		
+		
+	}
+
+}
