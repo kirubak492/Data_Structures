510093d112d914d433e553c3accdd4ee20c4d33d
kirubak492 <kirubak492@gmail.com>
2023-09-07 22:57:49 +0530

    bubble sort program

--- Bubble_sort.java
+++ Bubble_sort.java
@@ -1,8 +1,36 @@
 import java.util.*;
-class Main
+
+class Bubblesort 
 {
-    public static void main(String[]args)
-    {
-        System.out.println("Kirubakaran");
-    }
+	static void bubble(int arr[])
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=0;j<arr.length-i-1;j++)
+			{
+				if(arr[j]>arr[j+1])
+				{
+					int temp=arr[j+1];
+					arr[j+1]=arr[j];
+					arr[j]=temp;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) 
+	{
+		
+	  Scanner sc=new Scanner(System.in);
+	  int n=sc.nextInt();
+	  int arr[]=new int[n];
+	  for(int i=0;i<n;i++)
+	  {
+		  arr[i]=sc.nextInt();
+	  }
+	  bubble(arr);
+      System.out.println(Arrays.toString(arr));
+
+	}
+
 }
